530cf6e2a0501397ab5a510af02b12060e976108
Yusuke Nojima <nojima@ynojima.com>
2016-08-05 02:30:27 +0900

    [example] use clang as C++ compiler

--- example/src/main.cc
+++ example/src/main.cc
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <memory>
 #include "answer.h"
 
 int main() {
+  auto p = std::make_unique<Answer>();
   std::cout << "the Answer to the Ultimate Question "
                "of Life, the Universe, and Everything = "
-            << Answer().UltimateQuestion() << std::endl;
+            << p->UltimateQuestion() << std::endl;
   return 0;
 }
