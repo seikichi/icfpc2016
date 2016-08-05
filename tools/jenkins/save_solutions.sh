2a4b2448464364ee96e6a64f228b0dbf66a99021
Seiichi KONDO <seikichi@kmc.gr.jp>
2016-08-05 23:02:48 +0900

    Update solution save job script

--- tools/jenkins/save_solutions.sh
+++ tools/jenkins/save_solutions.sh
@@ -12,6 +12,5 @@ timestamp=$(date +%s)
 # solver 増やしたらここに追記
 for solver in centering rectangle multirectangle; do
     mkdir -p ${DATA_DIR}/${solver}
-    ./tools/calc_all_resemblances.py ${solver}/${solver} > ${DATA_DIR}/${solver}/${timestamp}.json
-    (cd ${DATA_DIR}/${solver} ; ln -fs "${timestamp}.json" "latest.json")
+    ./tools/calc_all_resemblances.py ${solver}/${solver} > ${solver}.json
 done
