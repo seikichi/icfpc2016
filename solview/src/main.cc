4f061cd07f19a1cced2ad9d1a4a5cdc1aa825e7f
MORI Shingo <cos@kmc.gr.jp>
2016-08-05 21:43:27 +0900

    remove comment

--- solview/src/main.cc
+++ solview/src/main.cc
@@ -95,48 +95,6 @@ int main(int argc, char *argv[]) {
       cout << "<path d=\"" << path << "\" fill=\"rgb(245,195,195)\" stroke=\"rgb(255,128,128)\" stroke-width=\"0.01\" />\n";
     }
   }
-
-  // string path;
-  // for (auto& polygon : silhouette) {
-  //   bool first = true;
-  //   for (auto& point : polygon) {
-  //     if (first) {
-  //       path += "M";
-  //       first = false;
-  //     } else {
-  //       path += "L";
-  //     }
-  //     path += to_string(point.real().get_d()) + " ";
-  //     path += to_string(point.imag().get_d()) + " ";
-  //   }
-  //   path += "z ";
-  // }
-  // cout << "<path d=\"" << path << "\" fill=\"rgb(245,195,195)\" />\n";
-  //
-  // path = "";
-  // bool first = true;
-  // for (auto& point : square) {
-  //   if (first) {
-  //     path += "M";
-  //     first = false;
-  //   } else {
-  //     path += "L";
-  //   }
-  //   path += to_string(point.real().get_d()) + " ";
-  //   path += to_string(point.imag().get_d()) + " ";
-  // }
-  // cout << "<path d=\"" << path
-  //      << " z\" fill=\"none\" "
-  //         "stroke=\"rgb(128,128,128)\" stroke-width=\"0.01\" />\n";
-  //
-  // for (auto& segment : skeleton) {
-  //   cout << "<line x1=\"" << segment.first.real().get_d() << "\" y1=\""
-  //        << segment.first.imag().get_d() << "\" x2=\""
-  //        << segment.second.real().get_d() << "\" y2=\""
-  //        << segment.second.imag().get_d()
-  //        << "\" style=\"stroke:rgb(246,147,147);stroke-width:0.01\" />\n";
-  // }
-  //
   cout << "</g>\n";
   cout << "</svg>\n";
   return 0;
