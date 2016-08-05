c069ed5222ffda2f815ce8b35c1d42f4993436f9
Seiichi KONDO <seikichi@kmc.gr.jp>
2016-08-05 23:26:46 +0900

    Add script for Jenkins to visualize solvers

---
+++ tools/jenkins/visualize_solvers.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -eu
+
+echo 'centering,rectangle,multirectangle,all' > total_resemblances.csv
+
+for solver in centering rectangle multirectangle; do
+    total=$(./tools/show_resemblances.py ${solver}.json | cut -d' ' -f3 | awk '{total = total + $1} END{print total}')
+    printf "%s," $total >> total_resemblances.csv
+done
+
+./tools/show_resemblances.py {centering,rectangle,multirectangle}.json | \
+    cut -d' ' -f3 | \
+    awk '{total = total + $1} END{print total}' >> total_resemblances.csv
+
+# count cleared (= resemblance = 1.0) problems 
+echo 'centering,rectangle,multirectangle,all' > completed_problems.csv
+
+for solver in centering rectangle multirectangle; do
+    total=$(./tools/show_resemblances.py ${solver}.json | grep -E ' 1(\.0*)?$' | wc -l)
+    printf "%s," $total >> completed_problems.csv
+done
+
+./tools/show_resemblances.py {centering,rectangle,multirectangle}.json | \
+    grep -E ' 1(\.0*)?$' | wc -l >> completed_problems.csv
