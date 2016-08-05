9be4e611045a7865fdf0d2a708f36376a368f50a
Yusuke Nojima <nojima@ynojima.com>
2016-08-05 15:03:18 +0900

    [lib] add library directory

--- lib/src/input.h
+++ lib/src/input.h
@@ -14,8 +14,12 @@ using Silhouette = std::vector<Polygon>;
 using Segment = std::pair<Point, Point>;
 using Skeleton = std::vector<Segment>;
 
+// 与えられた文字列を分数として読む
 mpq_class ReadFraction(std::string s);
+// 与えられた座標を読む
 Point ReadPoint(std::string s);
-Silhouette ReadSilhouette();
-Skeleton ReadSkeleton();
 
+// 標準入力からsilhouetteを読み込む
+Silhouette ReadSilhouette();
+// 標準入力からskeletonを読み込む
+Skeleton ReadSkeleton();
