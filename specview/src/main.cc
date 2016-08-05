4824ce0ed6b2a841daaf775c5d7e2b2df2c35077
Yusuke Nojima <nojima@ynojima.com>
2016-08-05 12:47:41 +0900

    [specview] translate

--- specview/src/main.cc
+++ specview/src/main.cc
@@ -5,7 +5,10 @@
 #include <vector>
 using namespace std;
 
-using Point = complex<double>;
+struct Point {
+  mpq_class x, y;
+  Point(const mpq_class& x, const mpq_class& y) : x(x), y(y) {}
+};
 using Polygon = vector<Point>;
 using Silhouette = vector<Polygon>;
 using Segment = pair<Point, Point>;
@@ -20,17 +23,15 @@ int ReadIntegerLine() {
   return stoi(line);
 }
 
-double ReadFraction(string s) {
+mpq_class ReadFraction(string s) {
   size_t slash = s.find('/');
   if (slash == string::npos) {
     mpz_class i(s, 10);
-    mpq_class q(i, 1);
-    return q.get_d();
+    return mpq_class(i, 1);
   } else {
     mpz_class numer(s.substr(0, slash), 10);
     mpz_class denom(s.substr(slash + 1), 10);
-    mpq_class q(numer, denom);
-    return q.get_d();
+    return mpq_class(numer, denom);
   }
 }
 
@@ -40,8 +41,8 @@ Point ReadPoint(string line) {
     cerr << "No comma in the line: " << line << "\n";
     exit(1);
   }
-  double x = ReadFraction(line.substr(0, comma));
-  double y = ReadFraction(line.substr(comma + 1));
+  mpq_class x = ReadFraction(line.substr(0, comma));
+  mpq_class y = ReadFraction(line.substr(comma + 1));
   return Point(x, y);
 }
 
@@ -85,16 +86,48 @@ Skeleton ReadSkeleton() {
   return skeleton;
 }
 
+void Canonicalize(Silhouette& silhouette, Skeleton& skeleton, Polygon& square) {
+  mpq_class xg(0), yg(0);
+  int count = 0;
+  for (auto& polygon : silhouette) {
+    for (auto& point : polygon) {
+      xg += point.x;
+      yg += point.y;
+      count++;
+    }
+  }
+  xg /= count;
+  yg /= count;
+  for (auto& polygon : silhouette) {
+    for (auto& point : polygon) {
+      point.x -= xg;
+      point.y -= yg;
+    }
+  }
+  for (auto& segment : skeleton) {
+    segment.first.x -= xg;
+    segment.first.y -= yg;
+    segment.second.x -= xg;
+    segment.second.y -= yg;
+  }
+  for (auto& point : square) {
+    point.x -= xg;
+    point.y -= yg;
+  }
+}
+
 int main() {
-  Silhouette silhoutte = ReadSilhouette();
+  Silhouette silhouette = ReadSilhouette();
   Skeleton skeleton = ReadSkeleton();
+  Polygon square = {Point(0, 0), Point(1, 0), Point(1, 1), Point(0, 1)};
+  Canonicalize(silhouette, skeleton, square);
 
   const string header =
-      R"(<svg xmlns="http://www.w3.org/2000/svg" viewBox="-1 -1 3 3">)";
+      R"(<svg xmlns="http://www.w3.org/2000/svg" viewBox="-1.5 -1.5 3 3">)";
   cout << header << "\n";
 
   string path;
-  for (auto& polygon : silhoutte) {
+  for (auto& polygon : silhouette) {
     bool first = true;
     for (auto& point : polygon) {
       if (first) {
@@ -103,19 +136,32 @@ int main() {
       } else {
         path += "L";
       }
-      path += to_string(point.real()) + " " + to_string(point.imag()) + " ";
+      path +=
+          to_string(point.x.get_d()) + " " + to_string(point.y.get_d()) + " ";
     }
     path += "z ";
   }
   cout << "<path d=\"" << path << "\" fill=\"rgb(245,195,195)\" />\n";
 
-  cout << "<path d=\"M0 0 L0 1 L1 1 L1 0 z\" fill=\"none\" "
+  path = "";
+  bool first = true;
+  for (auto& point : square) {
+    if (first) {
+      path += "M";
+      first = false;
+    } else {
+      path += "L";
+    }
+    path += to_string(point.x.get_d()) + " " + to_string(point.y.get_d()) + " ";
+  }
+  cout << "<path d=\"" << path
+       << " z\" fill=\"none\" "
           "stroke=\"rgb(128,128,128)\" stroke-width=\"0.01\" />\n";
 
   for (auto& segment : skeleton) {
-    cout << "<line x1=\"" << segment.first.real() << "\" y1=\""
-         << segment.first.imag() << "\" x2=\"" << segment.second.real()
-         << "\" y2=\"" << segment.second.imag()
+    cout << "<line x1=\"" << segment.first.x.get_d() << "\" y1=\""
+         << segment.first.y.get_d() << "\" x2=\"" << segment.second.x.get_d()
+         << "\" y2=\"" << segment.second.y.get_d()
          << "\" style=\"stroke:rgb(246,147,147);stroke-width:0.01\" />\n";
   }
 
