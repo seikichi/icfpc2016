6ff6af56e98a8061413e6abcc35b4e87704ce998
MORI Shingo <cos@kmc.gr.jp>
2016-08-05 19:31:19 +0900

    speed up for ConveHull

--- scoring/src/geometry.h
+++ scoring/src/geometry.h
@@ -64,7 +64,7 @@ std::vector<std::complex<T>> ConvexHull(std::vector<std::complex<T>> ps) {
   int n = ps.size();
   int k = 0;
   sort(ps.begin(), ps.end());
-  std::vector<Point> ch(2 * n);
+  std::vector<std::complex<T>> ch(2 * n);
   for (int i = 0; i < n; ch[k++] = ps[i++])
     while (k >= 2 && ccw(ch[k - 2], ch[k - 1], ps[i]) <= 0) --k;
   for (int i = n - 2, t = k + 1; i >= 0; ch[k++] = ps[i--])
