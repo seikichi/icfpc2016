cb58da368327d3d1f17d2c09ea61871839583a44
Seiichi KONDO <seikichi@kmc.gr.jp>
2016-08-05 23:05:16 +0900

    Update solution save job script

--- tools/jenkins/save_solutions.sh
+++ tools/jenkins/save_solutions.sh
@@ -7,10 +7,7 @@ for dir in rectangle multirectangle scoring; do
     (cd ${dir} ; make);
 done
 
-timestamp=$(date +%s)
-
 # solver 増やしたらここに追記
 for solver in centering rectangle multirectangle; do
-    mkdir -p ${DATA_DIR}/${solver}
     ./tools/calc_all_resemblances.py ${solver}/${solver} > ${solver}.json
 done
