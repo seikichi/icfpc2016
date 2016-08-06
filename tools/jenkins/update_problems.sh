1efe6294c2c982d64ecab6c23c38b6288db173a7
Seiichi KONDO <seikichi@kmc.gr.jp>
2016-08-06 13:51:16 +0900

    Update update-problems script

--- tools/jenkins/update_problems.sh
+++ tools/jenkins/update_problems.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+git checkout master
+
 ./tools/download_problems.py
 
-(cd specview; make clean ; make)
+(cd specview; make)
 
 for f in problems/*.in; do
     id=$(echo $f | sed -E 's@problems/([[:digit:]]+).in@\1@')
     ./specview/specview < problems/$id.in > problems/$id.svg
 done
 
-git pull
-git add problems/*.in problems/*.svg
-git commit -m 'Update problems (by Jenkins)'
-git push origin master
+if git diff --name-only | grep problems/ > /dev/null ; then
+    git add problems/*.in problems/*.svg
+    git commit -m 'Update problems (by Jenkins)'
+fi
