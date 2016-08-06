671f7a02ea5dbd8f62f00dda6decff143b950b44
Yusuke Nojima <nojima@ynojima.com>
2016-08-06 14:24:48 +0900

    [tools] parallelize calc_all_resemblances.py

--- tools/calc_all_resemblances.py
+++ tools/calc_all_resemblances.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import json
+import multiprocessing
+import os.path
 import re
 import subprocess
 import sys
@@ -8,46 +10,63 @@ import tempfile
 import time
 from pathlib import Path
 
-if len(sys.argv) != 2:
-    print('Usage: {} solver_path'.format(sys.argv[0]), file=sys.stderr)
-    sys.exit(1)
 
-solver_path = sys.argv[1]
-project_path = Path(__file__).parents[1]
-problems_path = project_path / 'problems'
-score_path = project_path / 'scoring/scoring'
-
-problems = {}
-globs = list(problems_path.glob('*.in'))
-for i, p in enumerate(globs):
-    problem_id = int(re.sub('\.in$', '', p.name))
+def solve(arg):
+    index, input_path, n_total, solver_path, score_path = arg
+    problem_id = int(re.sub('\.in$', '', input_path.name))
 
     print('Processing problem_id = {} ({}, {} / {})'.
-          format(problem_id, solver_path, i + 1, len(globs)), file=sys.stderr)
+          format(problem_id, solver_path, index + 1, n_total), file=sys.stderr)
 
     resemblance = float('nan')
     solution = None
 
     with tempfile.NamedTemporaryFile() as temp:
         try:
-            command = '{} < {}'.format(solver_path, p)
+            command = '{} < {}'.format(solver_path, input_path)
             solution = subprocess.check_output(command, shell=True)
             temp.write(solution)
             temp.flush()
 
-            command = '{} {} {}'.format(score_path, p, temp.name)
+            command = '{} {} {}'.format(score_path, input_path, temp.name)
             output = subprocess.check_output(command, shell=True)
             resemblance = float(output)
         except:
             print('Unexpected error:', sys.exc_info()[0], file=sys.stderr)
 
-    problems[problem_id] = {
-        'resemblance': resemblance,
-        'solution': solution.decode('utf-8'),
-    }
+    return problem_id, resemblance, solution
 
-print(json.dumps({
-    'problems': problems,
-    'solver_path': solver_path,
-    'timestamp': time.time(),
-}))
+
+def main():
+    if len(sys.argv) != 2:
+        print('Usage: {} solver_path'.format(sys.argv[0]), file=sys.stderr)
+        sys.exit(1)
+
+    solver_path = sys.argv[1]
+    project_path = Path(os.path.abspath(__file__)).parents[1]
+    problems_path = project_path / 'problems'
+    score_path = project_path / 'scoring/scoring'
+
+    problems = {}
+
+    with multiprocessing.Pool() as pool:
+        globs = list(problems_path.glob('*.in'))
+        args = [(i, p, len(globs), solver_path, score_path)
+                for i, p in enumerate(globs)]
+        results = pool.map(solve, args)
+
+        for problem_id, resemblance, solution in results:
+            problems[problem_id] = {
+                'resemblance': resemblance,
+                'solution': solution.decode('utf-8'),
+            }
+
+    print(json.dumps({
+        'problems': problems,
+        'solver_path': solver_path,
+        'timestamp': time.time(),
+    }))
+
+
+if __name__ == '__main__':
+    main()
