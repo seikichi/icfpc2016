d3178a2d11473c52539aaf85847e332aaf4df698
Seiichi KONDO <seikichi@kmc.gr.jp>
2016-08-08 01:40:43 +0900

    Enhance update script so that it do not calc 1.0 problems

--- tools/jenkins/run_solver.sh
+++ tools/jenkins/run_solver.sh
@@ -2,5 +2,6 @@
 
 set -eux
 
-(cd scoring ; make)
-./tools/update_solutions.py "${1}" "${1}.json"
+(cd scoring ; make clean ; make)
+curl 'http://104.199.162.10/perfect_problem_ids' > perfect_problem_ids
+./tools/update_solutions.py "${1}" "${1}.json" perfect_problem_ids
